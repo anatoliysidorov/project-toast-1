8151e58c97957defd4d3f8ae8a896ce4dfda4142
= <=>
2024-06-01 20:58:17 +0300

    Exercise 3

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -5,12 +5,67 @@ import Button from "../Button";
 import styles from "./ToastPlayground.module.css";
 
 import Toast from "../Toast";
+import ToastShelf from "../ToastShelf";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
+function useToggle(initialValue = false) {
+  if (typeof initialValue !== "boolean") {
+    console.warn("Invalid type for useToggle");
+  }
+
+  const [value, setValue] = React.useState(initialValue);
+
+  const toggleValue = React.useCallback(() => {
+    setValue((currentValue) => !currentValue);
+  }, []);
+
+  return [value, toggleValue];
+}
+
 function ToastPlayground() {
-  const [message, setMessage] = React.useState("");
-  const [variant, setVariant] = React.useState("");
+  const [message, setMessage] = React.useState("test");
+  const [variant, setVariant] = React.useState("notice");
+
+  const [toasts, setToasts] = React.useState([]);
+
+  function removeItem(id) {
+    const newArr = [...toasts].filter((toast) => {
+      return toast.id !== id;
+    });
+    setToasts(newArr);
+  }
+
+  function pushToast() {
+    const newArr = [...toasts];
+    newArr.push({
+      id: crypto.randomUUID(),
+      message: message,
+      variant: variant,
+    });
+    setToasts(newArr);
+  }
+
+  function submitForm(e) {
+    e.preventDefault();
+    pushToast();
+  }
+
+  /*
+  React.useEffect(() => {
+    function handleEscape(event) {
+      if (event.code === "Escape" && isModalOpen) {
+        toggleIsModalOpen();
+      }
+    }
+
+    window.addEventListener("keydown", handleEscape);
+
+    return () => {
+      window.removeEventListener("keydown", handleEscape);
+    };
+  }, [isModalOpen, toggleIsModalOpen]);
+  */
 
   return (
     <div className={styles.wrapper}>
@@ -18,10 +73,9 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      <ToastShelf toastList={toasts} removeItem={removeItem} />
 
-      <Toast></Toast>
-
-      <div className={styles.controlsWrapper}>
+      <form className={styles.controlsWrapper} onSubmit={submitForm}>
         <div className={styles.row}>
           <label
             htmlFor="message"
@@ -33,6 +87,7 @@ function ToastPlayground() {
           <div className={styles.inputWrapper}>
             <textarea
               id="message"
+              required
               className={styles.messageInput}
               value={message}
               onChange={(event) => {
@@ -47,7 +102,6 @@ function ToastPlayground() {
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             {VARIANT_OPTIONS.map((option) => {
               const id = `variant-${option}`;
-              const val = `${option}`;
               return (
                 <label htmlFor={id} key={id}>
                   <input
@@ -68,10 +122,10 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={submitForm}>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import Toast from '../Toast';
-import styles from './ToastShelf.module.css';
+import Toast from "../Toast";
+import styles from "./ToastShelf.module.css";
 
-function ToastShelf() {
+function ToastShelf({ toastList, removeItem }) {
+  const tList = [...toastList];
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {tList.map((toast) => (
+        <li className={styles.toastWrapper} key={toast.id}>
+          <Toast
+            id={toast.id}
+            message={toast.message}
+            variant={toast.variant}
+            handleDismiss={removeItem}
+          />
+        </li>
+      ))}
     </ol>
   );
 }
